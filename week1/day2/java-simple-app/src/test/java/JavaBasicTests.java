9d399053a270e82fe50bd8c77aaf89a8224f0896
tejaswini.r <tejaswini.r@collabera.com>
2022-01-04 12:41:36 +0530

    java prgm1

---
+++ week1/day2/java-simple-app/src/test/java/JavaBasicTests.java
@@ -0,0 +1,19 @@
+public class JavaBasicTests {
+
+    public void testVariable() {
+        int age = 10;
+        float sal = 20.6f;
+        double annualSal = 56.56;
+        char ch = 'a';
+        boolean isManager = false;
+        byte byt = 10;
+        short sh = 10;
+        long lng = 46464646464646L;
+        String nm = "android";
+    }
+
+    public void testMethods() {
+
+    }
+}
+
