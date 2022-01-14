06e1e55f649e509f25a8d8961052a05c163400bd
tejaswini.r <tejaswini.r@collabera.com>
2022-01-14 11:14:34 +0530

    unit testing : rest layer

---
+++ week2/day5/unit-testing-demo/src/main/java/com/ani/testing/UnitTestingDemoApplication.java
@@ -0,0 +1,12 @@
+package com.ani.testing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+ public class UnitTestingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitTestingDemoApplication.class, args);
+	}
+}
