b256e52a4305365285c228355c00b941ebb7e5f6
tejaswini.r <tejaswini.r@collabera.com>
2022-01-20 10:40:37 +0530

    initial commit

---
+++ week3/day4/code-quality/src/main/java/com/ani/sonar/domain/Msg.java
@@ -0,0 +1,13 @@
+package com.ani.sonar.domain;
+
+public class Msg {
+    private String mobile;
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+}
---
+++ week3/day4/code-quality/src/main/java/com/ani/sonar/dto/Msg.java
@@ -0,0 +1,13 @@
+package com.ani.sonar.dto;
+
+public class Msg {
+    private String mobile;
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+}
