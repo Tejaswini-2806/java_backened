d039e68684109e62cc04b035b3ddf801ea2d211a
tejaswini.r <tejaswini.r@collabera.com>
2022-01-24 14:53:38 +0530

    mvc :demo

---
+++ week2/day2/mvc-demo/src/main/java/com/ani/mvc/MvcDemoApplication.java
@@ -0,0 +1,14 @@
+package com.ani.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcDemoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MvcDemoApplication.class, args);
+	}
+
+}
