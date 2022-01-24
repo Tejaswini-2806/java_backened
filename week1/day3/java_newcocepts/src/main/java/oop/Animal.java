9e3b5acedd2a9bec4e543b305fa652abe90937a3
tejaswini.r <tejaswini.r@collabera.com>
2022-01-24 15:06:12 +0530

    opp concept

--- week1/day3/java_newcocepts/src/main/java/oop/Animal.java
+++ week1/day3/java_newcocepts/src/main/java/oop/Animal.java
@@ -1,5 +1,34 @@
 package oop;
+import oop.Animal;
+import oop.Tiger;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 
 public abstract class Animal {
-    abstract public String walk(); // this is incomplete
+    @Test
+    void testAnimalObject() {
+//        Animal animal = new Animal();
+        Animal animal = new Tiger();
+        Assertions.assertNotNull(animal);
+    }
+
+    @Test
+    void testTigerWalking() {
+        var tiger = new Tiger();
+
+        var expected = "Tiger is walking";
+        var actual = tiger.walk();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void testTigerLocation() {
+        var tiger = new Tiger();
+        var expected = true;
+        var actual = tiger.checkLocation();
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
